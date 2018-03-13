e5bc719aebac9f82b17033df8b6fd067001d5f02
agnynureza <rezaagny@gmail.com>
2018-03-13 20:26:40 +0700

    ecommerce

---
+++ vue.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+      carousels: [{
+          images: "./images/alpstein_massif_swiss_alps-wallpaper-1920x1080.jpg"
+      },{
+          images: "./images/boat_reflection-wallpaper-1600x900.jpg"
+      }]
+    }
+  })
