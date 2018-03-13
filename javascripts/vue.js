607586398703df2a1e4d3938fd53e5a9342fd904
FTeddy <ainsophoure@gmail.com>
2018-03-13 13:41:43 +0700

    vue setup complete

---
+++ javascripts/vue.js
@@ -0,0 +1,44 @@
+let carousel_banner = [
+  {imgUrl: './pictures/banners/g1-banner.jpg'},
+  {imgUrl: './pictures/banners/g2-banner.jpg'},
+  {imgUrl: './pictures/banners/g3-banner.jpg'}
+]
+
+let promo_banner = [
+  {
+    imgUrl: './pictures/promo/promo_1.jpg'
+  },
+  {
+    imgUrl: './pictures/promo/promo_1.jpg'
+  },
+]
+
+let cakes = [
+  {
+    imgUrl: './pictures/cakes/thumb_1.jpg',
+    title: 'cake name',
+    price: '$30'
+  },
+  {
+    imgUrl: './pictures/cakes/thumb_2.jpg',
+    title: 'cake name',
+    price: '$30'
+  },
+  {
+    imgUrl: './pictures/cakes/thumb_3.jpg',
+    title: 'cake name',
+    price: '$30'
+  },
+  {
+    imgUrl: './pictures/cakes/thumb_4.jpg',
+    title: 'cake name',
+    price: '$30'
+  }
+]
+
+new Vue({
+  el: '#vueApp',
+  data: {
+    title: 'CakePlaza'
+  }
+})
