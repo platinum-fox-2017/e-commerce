c28fa7b831838586f20de11f12e57eeb7f4014d6
FTeddy <ainsophoure@gmail.com>
2018-03-13 17:09:44 +0700

    added vue code to order to cart

--- javascripts/vue.js
+++ javascripts/vue.js
@@ -1,10 +1,16 @@
 let carousel_banner = [
-  {imgUrl: './pictures/banners/g1-banner.jpg'},
-  {imgUrl: './pictures/banners/g2-banner.jpg'},
-  {imgUrl: './pictures/banners/g3-banner.jpg'}
+  { imgUrl: './pictures/banners/g1-banner.jpg',
+    class: 'carousel-item active'
+  },
+  { imgUrl: './pictures/banners/g2-banner.jpg',
+    class: 'carousel-item'
+  },
+  {imgUrl: './pictures/banners/g3-banner.jpg',
+    class: 'carousel-item'
+  }
 ]
 
-let promo_banner = [
+let promo_banners = [
   {
     imgUrl: './pictures/promo/promo_1.jpg'
   },
@@ -16,29 +22,98 @@ let promo_banner = [
 let cakes = [
   {
     imgUrl: './pictures/cakes/thumb_1.jpg',
-    title: 'cake name',
-    price: '$30'
+    title: 'Ombre Cake',
+    price: '$50'
   },
   {
     imgUrl: './pictures/cakes/thumb_2.jpg',
-    title: 'cake name',
-    price: '$30'
+    title: 'Korean Blossom',
+    price: '$60'
   },
   {
     imgUrl: './pictures/cakes/thumb_3.jpg',
-    title: 'cake name',
-    price: '$30'
+    title: 'Decorated Cupcake',
+    price: '$15'
   },
   {
     imgUrl: './pictures/cakes/thumb_4.jpg',
-    title: 'cake name',
-    price: '$30'
+    title: 'Special Cupcake',
+    price: '$20'
   }
 ]
 
 new Vue({
   el: '#vueApp',
   data: {
-    title: 'CakePlaza'
+    title: 'CakePlaza',
+    cart: [],
+    carousel_banner: carousel_banner,
+    promo_banners: promo_banners,
+    cakes: cakes
+  },
+  computed: {
+    cartSize : function() {
+      return this.cart.length;
+    },
+    grandTotal: function() {
+      let grandTotal = 0;
+      this.cart.forEach(item => {
+        let priceNum = Number(item.price.slice(1));
+        let total = item.quantity * priceNum;
+        grandTotal += total;
+      })
+      console.log(grandTotal);
+      return grandTotal;
+    }
+  },
+  methods: {
+    addItem: function(title, price, url) {
+      let newProduct = {
+        title: title,
+        price: price,
+        imgUrl: url,
+        quantity: 1
+      };
+      // console.log(newProduct);
+      let search = this.cart.find(product => {
+        return product.title === newProduct.title;
+      })
+      if (search === undefined) {
+        this.cart.push(newProduct);
+        console.log(this.cart);
+        // console.log(this.cart[0]);
+      } else {
+        let index = this.cart.findIndex(data => {
+          return data.title == newProduct.title
+        });
+        // console.log(index);
+        this.cart[index].quantity += 1;
+        console.log(this.cart);
+      }
+    },
+    totalPrice: function(price, quantity) {
+      let priceNum = Number(price.slice(1));
+      let total = priceNum * quantity;
+      return '$ '+ total;
+    },
+    cartAddItem: function(title) {
+      let index = this.cart.findIndex(data => {
+        return data.title == title
+      });
+      this.cart[index].quantity += 1;
+    },
+    cartRemoveItem: function(title) {
+      let index = this.cart.findIndex(data => {
+        return data.title == title
+      });
+      this.cart[index].quantity -= 1;
+    },
+    cartRemoveAll: function(title) {
+      let index = this.cart.findIndex(data => {
+        return data.title == title
+      });
+      this.cart.splice(index, 1);
+    }
   }
+
 })
