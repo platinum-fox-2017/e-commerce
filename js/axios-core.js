b70716238c06c9b1844f1f5d5d1de0df0d3b24b1
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-17 22:18:46 +0700

    Change URL Base condition axios

--- js/axios-core.js
+++ js/axios-core.js
@@ -1,3 +1,13 @@
-var request = axios.create({
-  baseURL: 'http://localhost:3000',
-});
+const hostname = document.location.hostname;
+
+if (hostname == 'localhost' || hostname == '127.0.0.0') {
+  var request = axios.create({
+    baseURL: 'http://localhost:3000',
+  });
+} else {
+
+  var request = axios.create({
+    baseURL: 'http://ekom-api.geekosta.com',
+  });
+
+}
