da9382c34777e1bb2736c96ea42fe45f0ee67e37
herby herado <herby.herado@gmail.com>
2018-03-25 11:50:13 +0700

    fix cart modal overlay

--- client/assets/js/cart-component.js
+++ client/assets/js/cart-component.js
@@ -1,7 +1,7 @@
 var cart = Vue.component('cart', {
   template:`
   <div class="modal" id="modal-id">
-      <a href="#close" class="modal-overlay" aria-label="Close"></a>
+      <a href="#close" class="modal-overlay" aria-label="Close" @click="closemodal()"></a>
       <div class="modal-container">
           <div class="modal-header">
           <a href="#close" class="btn btn-clear float-right" aria-label="Close" @click="closemodal()"></a>
