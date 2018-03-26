e83037fbbfa8b0d4831bcce879321a80f990f07f
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-03-26 15:21:01 +0700

    transaction finish

--- client/assets/scripts/main.js
+++ client/assets/scripts/main.js
@@ -2,31 +2,58 @@ var app = new Vue({
   el: "#app",
   data: {
     items: [],
-    carts: [],
-    cartTotal: 0,
-    token_jwt: "",
+    carts: JSON.parse(localStorage.getItem("carts")) || [],
+    cartTotal: JSON.parse(localStorage.getItem("cartTotal")) || null,
+    token_jwt: null,
     email: null,
     password: null,
     access_token: null,
-    admin_jwt: null
+    admin_jwt: null,
+    name: null,
+    price: null,
+    description: null,
+    file: '',
+    orders: []
   },
-  created() {
-    let self = this;
+  mounted() {
+    let self = this
     axios.get(server + "/api/items").then(values => {
-      self.items = values.data.items;
-      self.token_jwt = localStorage.getItem("token_jwt");
-      self.access_token = localStorage.getItem("access_token");
-      self.admin_jwt = localStorage.getItem("admin_jwt");
-    });
-  },
-  computed: {
-    PrintPrice: function() {
-      return "ssss";
-    }
+      self.items = values.data.items
+      self.token_jwt = localStorage.getItem("token_jwt")
+      self.access_token = localStorage.getItem("access_token")
+      self.admin_jwt = localStorage.getItem("admin_jwt")
+    })
+
+    axios.get(server + "/api/alltransaction",{
+      headers: {
+        userId: localStorage.getItem("token_jwt")
+      }
+    }).then((response)=>{
+      self.orders = response.data.datas
+    })
   },
   methods: {
+    handleFileUpload(){
+      this.file = this.$refs.file.files[0]
+    },
+    addItem: function () {
+      let formData = new FormData()
+      formData.append('file', this.file)
+      formData.append('name', this.name)
+      formData.append('description', this.description)
+      formData.append('price', this.price)
+      axios.post(server + '/api/admin/additem',
+        formData,
+        { headers: {
+            'Content-type': 'multipart/form-data',
+          }
+        }
+      ).then((response)=>{
+        alert(response.data.message)
+      })
+    },
     logoutadmin: function() {
-      localStorage.clear();
+      localStorage.clear()
       window.location = "/index.html"
     },
     adminlogin: function(email, password) {
@@ -36,21 +63,23 @@ var app = new Vue({
           password: password
         })
         .then(response => {
-          localStorage.setItem("admin_jwt", response.data.token);
-          let self = this;
-          this.admin_jwt = localStorage.getItem("admin_jwt");
-        });
+          localStorage.setItem("admin_jwt", response.data.token)
+          let self = this
+          this.admin_jwt = localStorage.getItem("admin_jwt")
+        })
     },
     addToCart: function(item) {
-      let carts = this.carts;
-      let cartTotal = this.cartTotal;
+      let carts = this.carts
+      let cartTotal = this.cartTotal
 
+      // check if item already at cart
       for (let i = 0; i < carts.length; i++) {
         if (carts[i]._id === item._id) {
-          carts[i].quantity++;
-          carts[i].total += item.price;
-          this.cartTotal += item.price;
-          return "";
+          carts[i].quantity++
+          carts[i].total += item.price
+          this.cartTotal += item.price
+          localStorage.setItem("carts", JSON.stringify(carts))
+          return localStorage.setItem("cartTotal", this.cartTotal)
         }
       }
 
@@ -61,22 +90,47 @@ var app = new Vue({
         _id: item._id,
         quantity: 1,
         total: item.price
-      };
-      carts.push(payload);
-      this.cartTotal += item.price;
+      }
+
+      carts.push(payload)
+      this.cartTotal += item.price
+      localStorage.setItem("carts", JSON.stringify(carts))
+      localStorage.setItem("cartTotal", this.cartTotal)
     },
     clearCart: function() {
-      this.carts = [];
-      this.cartTotal = 0;
+      this.carts = []
+      this.cartTotal = 0
+      localStorage.removeItem("carts")
+      localStorage.removeItem('cartTotal')
     },
     clearOneCart: function(cart) {
-      let carts = this.carts;
+      let carts = this.carts
       for (let i = 0; i < carts.length; i++) {
-        if (cart.sku === carts[i].sku) {
-          carts.splice(i, 1);
-          this.cartTotal -= cart.total;
+        if (cart._id === carts[i]._id) {
+          carts.splice(i, 1)
+          this.cartTotal -= cart.total
+          localStorage.setItem("carts", JSON.stringify(carts))
+          localStorage.setItem("cartTotal", this.cartTotal)
         }
       }
+    },
+    checkout : function () {
+      let self = this
+      let carts = JSON.parse(localStorage.getItem("carts"))
+      axios.post(server + '/api/checkout', {
+        total: localStorage.getItem("cartTotal"),
+        carts: carts
+      },{
+        headers: {
+          userId: localStorage.getItem("token_jwt")
+        }
+      })
+      .then((response)=>{
+        localStorage.removeItem("cartTotal")
+        localStorage.removeItem("carts")
+        self.carts = []
+        self.cartTotal = ''
+      })
     }
   }
-});
+})
